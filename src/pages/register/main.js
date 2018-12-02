f740c763bb72b5ca5871e05c79a4ff7a88e5ef3b
xurundong <1039279239@qq.com>
2018-12-02 11:36:18 +0800

    测试一下

---
+++ src/pages/register/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 10392 on 2018/12/2.
+ */
+import Vue from 'vue'
+import Register from  './register.vue'
+
+const register = new Vue(Register)
+register.$mount()
---
+++ src/pages/register/register.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="registerContainer">
+    注册页面
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {}
+    },
+    beforeMount(){ // 获取用户的登录信息
+
+    },
+    methods: {}
+  }
+</script>
+
+<style>
+  page {
+    background-color: #8ed145;
+  }
+
+</style>
+
